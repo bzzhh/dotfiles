85c9fb2c13dc6c4319db9e6b2d58112f40d2656f
bzzhh <sjehann@gmail.com>
2021-11-08 21:45:24 +0100

    upgrade lvim conf

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,57 +1,13 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.lint_on_save = true
 lvim.colorscheme = "zenburn"
 
--- settings
-vim.opt.wrap = true
-
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
---   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
---   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
-
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
-}
-
--- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
@@ -59,95 +15,56 @@ lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
 -- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
-
--- set a formatter if you want to override the default lsp one (if it exists)
-
--- dap
--- lvim.builtin.dap.active = true
--- local dap = require('dap')
-
--- dap.adapters.php = {
---   type = 'executable',
---   command = 'node',
---   args = {os.getenv('HOME') .. '/.local/share/nvim/dapinstall/php/vscode-php-debug/out/phpDebug.js'}
--- }
--- dap.configurations.php = {
---   {
---     type = 'php',
---     request = 'launch',
---     name = 'Listen for Xdebug',
---     port = 9000
---   }
--- }
-
--- dap.adapters.go = {
---   type = 'executable';
---   command = 'node';
---   args = {os.getenv('HOME') .. '/.local/share/nvim/dapinstall/vscode-go/dist/debugAdapter.js'};
--- }
--- dap.configurations.go = {
---   {
---     type = 'go';
---     name = 'Debug';
---     request = 'launch';
---     showLog = false;
---     program = "${file}";
---     dlvToolPath = vim.fn.exepath('dlv')
---   },
--- }
-
-local formatters = require "lvim.lsp.null-ls.formatters"
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup({{exe = "eslint_d", filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" } }})
-lvim.lang.markdown = {formatters = {{ exe = "markdownlint" }}, linters = {{ exe = "markdownlint" }}}
-lvim.lang.go.formatters = {{ exe = "gofumpt" }, { exe = "golines" }}
-lvim.lang.php.formatters = {{ exe = "phpcsfixer" }}
-lvim.lang.python = {formatters = {{exe = "black"}}, linters = {{exe = "flake8"}}}
-
-formatters.setup({{exe = "prettier", filetypes = {
-  "htmldjango.twig",
-  "javascript",
-  "javascriptreact",
-  "php",
-  "typescript",
-  "typescriptreact",
-} }})
-
 lvim.lsp.on_attach_callback = function(client, _)
   if client.name == "tsserver" then
     client.resolved_capabilities.document_formatting = false
     client.resolved_capabilities.document_range_formatting = false
   end
 end
+
+-- formatters.setup {
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup({
+  { exe = "prettier", filetypes = {
+    "htmldjango.twig",
+    "javascript",
+    "javascriptreact",
+    "php",
+    "typescript",
+    "typescriptreact",
+  } },
+  { exe = "markdownlint", filetypes = { "markdown" } },
+  { exe = "gofumpt", filetypes = { "go" } },
+  { exe = "golines", filetypes = { "go" } },
+  { exe = "phpcsfixer", filetypes = { "php" } },
+  { exe = "black", filetypes = { "python" } },
+})
+
+-- linters.setup {
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup({
+  { exe = "eslint_d", filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" } },
+  { exe = "markdownlint", filetypes = { "markdown" } },
+  { exe = "flake8", filetypes = { "python" } },
+})
 
 -- Additional Plugins
 lvim.plugins = {
@@ -174,6 +91,7 @@ lvim.plugins = {
     {"~/code/ligo/tools/vim/ligo/start/ligo"},
 }
 
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
   { "BufNewFile,BufRead", "*.pcss", "set filetype=css" }
 }
