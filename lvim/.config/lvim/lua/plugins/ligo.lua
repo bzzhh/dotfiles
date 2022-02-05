39c2683cf551ac77067d1edbb49d27f68acc6d33
Steven <sjehann@gmail.com>
2022-02-05 12:29:07 +0100

    fix lvim

--- lvim/.config/lvim/lua/plugins/ligo.lua
+++ lvim/.config/lvim/lua/plugins/ligo.lua
@@ -1,28 +1,32 @@
 local M = {}
-local configs = require("lspconfig.configs")
-local util = require("lspconfig.util")
-local ft = require("Comment.ft")
 
-ft({ "ligo", "mligo" }, { "//%s" })
+M.config = function()
+	local configs = require("lspconfig.configs")
+	local util = require("lspconfig.util")
+	local ft = require("Comment.ft")
 
-configs.ligolang = {
-	default_config = {
-		cmd = { "ligo-squirrel" },
-		filetypes = { "ligo", "mligo", "religo" },
-		root_dir = util.find_git_ancestor,
-	},
-	docs = {
-		description = [[
-https://gitlab.com/ligolang/ligo/-/tree/dev/tools/lsp
-A language server for all three dialects of LIGO.
-]],
-	},
-}
+	ft({ "ligo", "mligo" }, { "//%s" })
 
-local opts = {
-	-- on_attach = require("user.lsp.handlers").on_attach,
-	-- capabilities = require("user.lsp.handlers").capabilities,
-}
+	configs.ligolang = {
+		default_config = {
+			cmd = { "ligo-squirrel" },
+			filetypes = { "ligo", "mligo", "religo" },
+			root_dir = util.find_git_ancestor,
+		},
+		docs = {
+			description = [[
+  https://gitlab.com/ligolang/ligo/-/tree/dev/tools/lsp
+  A language server for all three dialects of LIGO.
+  ]],
+		},
+	}
 
-require("lspconfig").ligolang.setup({ opts })
+	local opts = {
+		-- on_attach = require("user.lsp.handlers").on_attach,
+		-- capabilities = require("user.lsp.handlers").capabilities,
+	}
+
+	require("lspconfig").ligolang.setup({ opts })
+end
+
 return M
