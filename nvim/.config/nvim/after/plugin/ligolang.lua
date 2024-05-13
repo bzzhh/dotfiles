51324e489cc2c3fbe56c1fa67d92ae58ab375ff3
steven j <sjehann@gmail.com>
2024-05-13 22:17:19 +0200

    format

--- nvim/.config/nvim/after/plugin/ligolang.lua
+++ nvim/.config/nvim/after/plugin/ligolang.lua
@@ -1,25 +1,25 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require "lsp-zero"
 
-local configs = require('lspconfig.configs')
-local util = require('lspconfig.util')
+local configs = require "lspconfig.configs"
+local util = require "lspconfig.util"
 
 local server_name = "ligo_ls"
 local cmd = { "ligo", "lsp" }
 local root_files = {
-    "ligo.json",
+  "ligo.json",
 }
 
 configs[server_name] = {
-    default_config = {
-        cmd = cmd,
-        filetypes = { "jsligo", "mligo" },
-        root_dir = util.root_pattern(root_files),
-        init_options = {
-            ligoLanguageServer = {
-                loggingVerbosity = "error",
-            },
-        },
+  default_config = {
+    cmd = cmd,
+    filetypes = { "jsligo", "mligo" },
+    root_dir = util.root_pattern(root_files),
+    init_options = {
+      ligoLanguageServer = {
+        loggingVerbosity = "error",
+      },
     },
+  },
 }
 
 lsp_zero.extend_lspconfig()
--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,47 +1,47 @@
 -- Learn the keybindings, see :help lsp-zero-keybindings
 -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-local lsp = require("lsp-zero")
+local lsp = require "lsp-zero"
 
-local lsp_zero = require("lsp-zero")
+local lsp_zero = require "lsp-zero"
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps { buffer = bufnr }
 end)
 
-require("mason").setup({})
-require("mason-lspconfig").setup({
-    -- Replace the language servers listed here
-    -- with the ones you want to install
-    ensure_installed = { "intelephense", "tsserver", "rust_analyzer" },
-    handlers = {
-        function(server_name)
-            require("lspconfig")[server_name].setup({})
-        end,
-    },
-    lua_ls = function()
-        local lua_opts = lsp_zero.nvim_lua_ls()
-        require("lspconfig").lua_ls.setup(lua_opts)
+require("mason").setup {}
+require("mason-lspconfig").setup {
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = { "intelephense", "tsserver", "rust_analyzer" },
+  handlers = {
+    function(server_name)
+      require("lspconfig")[server_name].setup {}
     end,
-})
+  },
+  lua_ls = function()
+    local lua_opts = lsp_zero.nvim_lua_ls()
+    require("lspconfig").lua_ls.setup(lua_opts)
+  end,
+}
 
 lsp.configure("yamlls", {
-    on_attach = function(_, _)
-        vim.keymap.set("n", "<leader>f", "<cmd>!yq -iP %<CR>")
-    end,
+  on_attach = function(_, _)
+    vim.keymap.set("n", "<leader>f", "<cmd>!yq -iP %<CR>")
+  end,
 })
 
 lsp.configure("ocamllsp", {
-    on_attach = function(_, bufnr)
-        if string.match(vim.api.nvim_buf_get_name(bufnr), ".re") then
-            vim.keymap.set("n", "<leader>f", "<cmd>!refmt --in-place %<CR>")
-        end
-    end,
+  on_attach = function(_, bufnr)
+    if string.match(vim.api.nvim_buf_get_name(bufnr), ".re") then
+      vim.keymap.set("n", "<leader>f", "<cmd>!refmt --in-place %<CR>")
+    end
+  end,
 })
 
 lsp.setup()
 
-vim.diagnostic.config({
-    virtual_text = true,
-})
+vim.diagnostic.config {
+  virtual_text = true,
+}
