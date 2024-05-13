509894072292510d35a2730bb89b0b3e701a1712
steven j <sjehann@gmail.com>
2024-05-14 00:45:06 +0200

    up

--- nvim/.config/nvim/lua/custom/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,14 +1,6 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      { dir = "~/plugins/tree-sitter-lua" },
-    },
     build = ":TSUpdate",
-    branch = "main",
-    lazy = false,
-    config = function()
-      require("custom.treesitter").setup()
-    end,
   },
 }
