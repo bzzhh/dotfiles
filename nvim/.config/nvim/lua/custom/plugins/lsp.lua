976c9790cede8e231ff1d6ab9f1200fb585a3b15
steven j <sjehann@gmail.com>
2024-06-01 16:13:41 +0200

    up conform conf

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -138,21 +138,17 @@ return {
 
       -- Autoformatting Setup
       require("conform").setup {
+        format_on_save = function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 3000, lsp_fallback = true }
+        end,
         formatters_by_ft = {
           lua = { "stylua" },
           php = { "prettier", "phpcbf" },
         },
       }
-
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        callback = function(args)
-          require("conform").format {
-            bufnr = args.buf,
-            lsp_fallback = true,
-            quiet = true,
-          }
-        end,
-      })
     end,
   },
 }
